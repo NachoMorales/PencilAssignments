36f62b1e81571c42e7249559f1aaff497f399c83
nacho_morales <ignacio@diproach.com>
2021-10-15 19:16:11 -0300

    Frontend: fabric js & firebase rtdb

--- frontend/src/app/core/base.component.ts
+++ frontend/src/app/core/base.component.ts
@@ -1,10 +1,10 @@
-import { OnInit, Directive, ElementRef } from '@angular/core';
+import { OnInit, Directive } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { PageService } from './page.service';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Settings } from '../app.settings';
 import { AuthService } from './auth.service';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
 
 @Directive({})
 export class BaseComponent implements OnInit {
@@ -12,29 +12,20 @@ export class BaseComponent implements OnInit {
   user!: firebase.default.User | null;
   country: any;
   settings = Settings;
-  global: any;
   
   constructor(
     public formBuilder: FormBuilder,
     public activatedRoute: ActivatedRoute,
-    public modalService: NgbModal,
     public pageService: PageService,
-    public authService: AuthService
+    public authService: AuthService,
+    public db: AngularFireDatabase
   ) {
     this.settings = this.pageService.settings;
-    this.authService.getLoggedUser().subscribe(res => this.user = res);
+    this.authService.getLoggedUser().subscribe(res => this.user = res); // To do: unsubscribe
   }
 
   ngOnInit() {
 
-  }
-
-  openModal(content: ElementRef) {
-    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result;
-  }
-
-  closeModal() {
-    this.modalService.dismissAll();
   }
 
 }
--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { AngularFireObject } from '@angular/fire/compat/database';
 import { fabric } from 'fabric';
+import { Observable } from 'rxjs';
 import { BaseComponent } from '../core/base.component';
-
 
 @Component({
   selector: 'app-home',
@@ -10,11 +11,49 @@ import { BaseComponent } from '../core/base.component';
 })
 export class HomeComponent extends BaseComponent {
 
+  color = '#43FA32';
   canvas!: fabric.Canvas;
+  savedCanvas!: string;
+  dbCanvasRef!: AngularFireObject<any>;
+  dbCanvas!: any;
 
   ngAfterViewInit() {
-    this.canvas = new fabric.Canvas('canvas');
-    this.canvas.setDimensions({ width: '100%', height: '100vh' }, { cssOnly: true });
+    this.dbCanvasRef = this.db.object('canvas');
+    // this.dbCanvasRef = this.db.object(this.user.uid); To do: usar el uid del usuario
+    this.dbCanvas = this.dbCanvasRef.valueChanges();
+    this.loadCanvas();
+  }
+  
+  loadCanvas() {
+    this.canvas = new fabric.Canvas('canvas', { isDrawingMode: true, width: 10000, height: 1000 });
+    this.canvas.setDimensions({ width: '100vw', height: '100vh' }, { cssOnly: true });
+    this.loadEvents();
+    this.handleColor();
+    this.restoreCanvas();
   }
 
+  loadEvents() {
+    // To Do: agregar evento on mouse up o alguno de esos que ejecute saveCanvas()
+  }
+
+  clearCanvas() {
+    this.canvas.clear();
+  }
+
+  handleColor() {
+    this.canvas.freeDrawingBrush.color = this.color;
+  }
+
+  saveCanvas() {
+    this.dbCanvasRef.set(this.canvas.toObject());
+  }
+
+  restoreCanvas() {
+    this.dbCanvas.subscribe((res: any) => {
+      console.log(res);
+      this.canvas.loadFromJSON(res, (objects: fabric.Object[]) => {
+        if (objects) this.canvas.add(...objects);
+      });
+    });
+  }
 }
